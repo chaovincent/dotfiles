4ad1cb1e442e7e3e2c81bd924fbff3dd827a37eb
Vincent Chao <vincechao.93@gmail.com>
2024-01-09 14:09:02 -0800

    Update sway setup script and config settings

--- scripts/sway.sh
+++ scripts/sway.sh
@@ -1,31 +1,32 @@
 #!/bin/sh
 
-# #==============#
-# # Package List #
-# #==============#
-# 
-# set -- \
-#     sway \
-#     swaylock \
-#     waybar \
-#     wdisplays \
-#     wl-clipboard \
-#     wofi
-# 
-# echo "Installing packages:\n  $@\n"
-# 
-# #==================#
-# # Install Packages #
-# #==================#
-# 
-# if command -v apt >/dev/null 2>&1; then
-#     sudo apt install -y $@
-# elif command -v yay >/dev/null 2>&1; then
-#     yay -S $@
-# elif command -v pacman >/dev/null 2>&1; then
-#     sudo pacman -S $@
-# fi
-# 
+#==============#
+# Package List #
+#==============#
+
+set -- \
+    fcitx5 \
+    sway \
+    swaylock \
+    waybar \
+    wdisplays \
+    wl-clipboard \
+    wofi
+
+echo "Installing packages:\n  $@\n"
+
+#==================#
+# Install Packages #
+#==================#
+
+if command -v apt >/dev/null 2>&1; then
+    sudo apt install -y $@
+elif command -v yay >/dev/null 2>&1; then
+    yay -S $@
+elif command -v pacman >/dev/null 2>&1; then
+    sudo pacman -S $@
+fi
+
 #===================#
 # Link config files #
 #===================#
