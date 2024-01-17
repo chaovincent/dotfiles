d6bd2a4f8a34c36864f59357124bedbe66edc3c7
Vincent Chao <vincechao.93@gmail.com>
2024-01-17 10:16:45 -0800

    Add XDG base dirs to environment

--- bash/bash.d/environment.bash
+++ bash/bash.d/environment.bash
@@ -3,8 +3,14 @@
 export EDITOR=vi
 export TERM=xterm-256color
 
-# Avoid clutter in home directory
-export XDG_CACHE_HOME=${HOME}/.cache
+# Define base user directories to reduce clutter
+export XDG_CACHE_HOME=${HOME}/.cache        # Where user-specific non-essential (cached) data should be written (analogous to /etc).
+export XDG_CONFIG_HOME=${HOME}/.config      # Where user-specific configurations should be written (analogous to /var/cache).
+export XDG_DATA_HOME=${HOME}/.local/share   # Where user-specific data files should be written (analogous to /usr/share).
+export XDG_STATE_HOME=${HOME}/.local/state  # Where user-specific state files should be written (analogous to /var/lib).
+export XDG_RUNTIME_DIR=/run/user/${UID}     # Used for non-essential, user-specific data files such as sockets, named pipes, etc.
+
 export CARGO_HOME=${XDG_CACHE_HOME}/cargo
-export JUPYTER_CONFIG_DIR=${XDG_CACHE_HOME}/jupyter
 export MLFLOW_RECIPES_EXECUTION_DIRECTORY=${XDG_CACHE_HOME}/mlflow/recipes
+
+export JUPYTER_CONFIG_DIR=${XDG_CONFIG_HOME}/jupyter
