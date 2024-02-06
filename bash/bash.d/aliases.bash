06b973241fd27e52aafc05aeecf27d21a4ccff11
Vincent Chao <vincechao.93@gmail.com>
2024-02-06 10:46:55 -0800

    Update bash aliases for ls and diff

--- bash/bash.d/aliases.bash
+++ bash/bash.d/aliases.bash
@@ -1,11 +1,13 @@
-alias ls='ls --color=auto'
 alias grep='grep --color=auto'
 alias fgrep='fgrep --color=auto'
 alias egrep='egrep --color=auto'
-alias tree="tree -C"
 
+alias ls='ls --color=auto --group-directories-first'
 alias ll='ls -alF'
+
 alias src="source ${HOME}/.bashrc"
+alias tree="tree -C"
+alias diffs="diff --suppress-common-lines"
 
 # Vim
 if [ -x "$(command -v nvim)" ]; then
