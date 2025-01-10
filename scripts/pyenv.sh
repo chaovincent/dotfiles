6b7f7f6b9eafd81b30fe52253925d2c87dad3b1a
Vincent Chao <vincechao.93@gmail.com>
2025-01-10 11:33:10 -0800

    Add packages for pyenv

--- scripts/pyenv.sh
+++ scripts/pyenv.sh
@@ -1,3 +1,22 @@
 #!/bin/sh
 
 curl https://pyenv.run | bash
+
+sudo apt install -y \
+    build-essential \
+    curl \
+    libbz2-dev \
+    libffi-dev \
+    liblzma-dev
+    libncursesw5-dev \
+    libreadline-dev \
+    libsqlite3-dev \
+    libssl-dev \
+    libxml2-dev \
+    libxmlsec1-dev \
+    llvm \
+    make \
+    tk-dev \
+    wget \
+    xz-utils \
+    zlib1g-dev \
