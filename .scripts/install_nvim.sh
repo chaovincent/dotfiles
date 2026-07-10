46b79ee8cfe1566ed310fa559474dd372c2c878e
Vincent Chao <vincechao.93@gmail.com>
2026-07-10 15:51:30 -0700

    Move nvim script to snap due to old ubuntu version (20)

--- .scripts/install_nvim.sh
+++ .scripts/install_nvim.sh
@@ -5,7 +5,5 @@
 #==========#
 
 if ! command -v nvim >/dev/null 2>&1; then
-    sudo add-apt-repository ppa:neovim-ppa/unstable
-    sudo apt update
-    sudo apt install -y neovim
+    sudo snap install nvim --classic
 fi
