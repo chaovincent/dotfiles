e5cf5c2dd6c3972825adeb39b0ce143513c3002d
Vincent Chao <vincechao.93@gmail.com>
2026-07-13 11:58:23 -0700

    Add keybind for renaming wezterm tabs

--- wezterm/.config/wezterm/wezterm.lua
+++ wezterm/.config/wezterm/wezterm.lua
@@ -9,4 +9,20 @@ config.colors = {
     selection_fg = 'none',
 }
 
+config.keys = {
+  {
+    key = 'E',
+    mods = 'CTRL|SHIFT',
+    action = wezterm.action.PromptInputLine {
+      description = 'Enter new name for tab',
+      action = wezterm.action_callback(function(window, pane, line)
+        if line then
+          window:active_tab():set_title(line)
+        end
+      end),
+    },
+  },
+}
+
+
 return config
