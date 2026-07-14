12840ac7d3138798d5e55e5e24c8306ea1d90a4a
Vincent Chao <vincechao.93@gmail.com>
2026-07-14 16:35:08 -0700

    Patch tree-sitter config to use master branch

--- nvim/.config/nvim/lua/plugins/tree-sitter.lua
+++ nvim/.config/nvim/lua/plugins/tree-sitter.lua
@@ -1,34 +1,42 @@
 return {
     "nvim-treesitter/nvim-treesitter",
+    branch = "master",
     build = ":TSUpdate",
     event = { "BufReadPost", "BufNewFile" },
-    main = "nvim-treesitter.configs",
-    opts = {
-        ensure_installed = {
-            "bash",
-            "c",
-            "cpp",
-            "dockerfile",
-            "gitattributes",
-            "gitignore",
-            "javascript",
-            "json",
-            "lua",
-            "markdown",
-            "python",
-            "toml",
-            "yaml",
-        },
-        sync_install = false,
-        highlight = {
-            enable = true,
-            additional_vim_regex_highlighting = true,
-        },
-        indent = {
-            enable = true,
-            disable = {
+    config = function()
+        local status_ok, configs = pcall(require, "nvim-treesitter.configs")
+        if not status_ok then
+            vim.notify("Treesitter is switching branches. Open :Lazy and press 'U' to sync!", vim.log.levels.WARN)
+            return
+        end
+
+        configs.setup({
+            ensure_installed = {
+                "bash",
+                "c",
+                "cpp",
+                "dockerfile",
+                "gitattributes",
+                "gitignore",
+                "javascript",
+                "json",
+                "lua",
+                "markdown",
+                "python",
+                "toml",
                 "yaml",
             },
-        },
-    },
+            sync_install = false,
+            highlight = {
+                enable = true,
+                additional_vim_regex_highlighting = true,
+            },
+            indent = {
+                enable = true,
+                disable = {
+                    "yaml",
+                },
+            },
+        })
+    end,
 }
